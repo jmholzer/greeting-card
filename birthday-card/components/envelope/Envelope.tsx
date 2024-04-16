483b755cfe8534f2b551d2eb15900fd9b0ac81d0
Jannic Holzer <jannic.holzer@gmail.com>
2024-04-16 09:51:29 +0200

    Add keyframes to animation, resize

--- birthday-card/components/envelope/Envelope.tsx
+++ birthday-card/components/envelope/Envelope.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 import styles from './envelope.module.css';
@@ -11,9 +10,17 @@ export default function Home() {
     setIsOpen(!isOpen);
   };
 
-  const envelopeAnimation = isOpen ? {
-    opacity: 0, y: 400, scale: 1.25, transition: { delay: 0.6, duration: 0.6, ease: [0.175, 0.885, 0.32, 1] }
-  } : {}
+  const envelopeAnimation = isOpen
+    ? {
+      opacity: [1, 1, 0],
+      y: [0, 400, 400],
+      transition: {
+        delay: 0.2,
+        duration: 5,
+        ease: [0.175, 0.885, 0.32, 1],
+      },
+    }
+    : {};
 
   return (
     <div className={styles.container}>
@@ -31,15 +38,19 @@ export default function Home() {
           <motion.div
             className={styles.card}
             initial={{ y: 0 }}
-            animate={isOpen ? { transition: { delay: 0.2, duration: 0.6, ease: [0.175, 0.885, 0.32, 1] } } : {}}
+            animate={
+              isOpen
+                ? {
+                  transition: {
+                    delay: 0.2,
+                    duration: 0.6,
+                    ease: [0.175, 0.885, 0.32, 1],
+                  },
+                }
+                : {}
+            }
             style={{ zIndex: 2 }}
-          >
-            <motion.div
-              className={styles.inner}
-              initial={{ rotate: 0, scale: 1 }}
-              animate={isOpen ? { rotate: -90, scale: 1.15, transition: { delay: 0.4, duration: 0.3, ease: [0.175, 0.885, 0.32, 1] } } : {}}
-            />
-          </motion.div>
+          />
           <motion.div
             key="envelopeFront"
             id="envelopeFront"
@@ -49,7 +60,6 @@ export default function Home() {
             animate={envelopeAnimation}
             style={{ zIndex: 3 }}
           />
-
         </div>
       </AnimatePresence>
     </div>
